e42e0514c017a01b3528129d8d316e951bbe703e
Zaytsev1991 <115064075+Zaytsev1991@users.noreply.github.com>
2023-02-04 09:38:19 +0300

    Delete 2.js

--- 2.js
+++
@@ -1,8 +0,0 @@
-//Cоздать функцию greeting, которая принимает имя и выводит приветствие, используя переданное имя, в консоль.
-//Необходимо у пользователя запросить имя и вызвать функцию greeting, передав туда данное значение.
-
-function greeting(yourName) {
-    console.log(`Hello ${yourName}`);
-}
-const name1 = prompt('Push your name ');
-greeting(name1);
